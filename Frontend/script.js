fb3cd14b6bc6f0432bca47b53bf3fc8550c8d60f
Lucas Blom <51747171+SkateFastEatTrash@users.noreply.github.com>
2022-10-05 21:34:40 +0200

    alle ToDo's einblenden lassen durch js

--- Frontend/script.js
+++ Frontend/script.js
@@ -3,28 +3,31 @@ const createCell = (cellText) => {
     cell.innerText = cellText;
     return cell
 }
-
 const addTask = (task) => {
     const display = document.getElementById("display");
     const tableRow = document.createElement("tr");
 
-    tableRow.append(
-        createCell(task.id),
-        createCell(task.title),
-        createCell(task.completed),
-    );
+    for(let inde = task.length; inde >= 0; inde--) {
+        inde--;
+        console.log(inde);
 
+        tableRow.append(
+            createCell(task[inde].id),
+            createCell(task[inde].title),
+            createCell(task[inde].completed)
+        )
+    }
     display.appendChild(tableRow);
-};
-
+}
 document.addEventListener("DOMContentLoaded", () => {
 
     fetch("http://localhost:3000/tasks")
-    .then((response) => {
-        alert(response.value);
+    .then((response)=> {
         return response.json();
     })
     .then((data) =>{
         addTask(data)
     })
+
 });
+// Alle ToDos Einblenden
