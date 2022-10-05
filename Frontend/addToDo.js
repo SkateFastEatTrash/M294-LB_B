7f301c1fd6648471d735610df63e264ba5a8d85b
Lucas Blom <51747171+SkateFastEatTrash@users.noreply.github.com>
2022-10-05 21:45:43 +0200

    New JS adding Todos

---
+++ Frontend/addToDo.js
@@ -0,0 +1,8 @@
+// ToDos Hinzufügen
+const _idInput = document.getElementById("id");
+const todoInput = document.getElementById("todo");
+const statusInput = document.getElementById("status");
+
+document.addEventListener("DOMContentLoaded", () => {
+    
+})
