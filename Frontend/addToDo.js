226b10a214d51d95babd14f5a920b8ddc8458a7e
Lucas Blom <51747171+SkateFastEatTrash@users.noreply.github.com>
2022-10-06 09:14:10 +0200

    addToDo.js deleted

--- Frontend/addToDo.js
+++
@@ -1,8 +0,0 @@
-// ToDos Hinzufügen
-const _idInput = document.getElementById("id");
-const todoInput = document.getElementById("todo");
-const statusInput = document.getElementById("status");
-
-document.addEventListener("DOMContentLoaded", () => {
-    
-})
