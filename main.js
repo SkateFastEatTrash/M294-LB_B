ae16bd6d080fd3db99259618247d04efe8ccd9f7
Lucas Blom <51747171+SkateFastEatTrash@users.noreply.github.com>
2022-10-07 11:40:56 +0200

    login wurde implementiert

--- main.js
+++ main.js
@@ -68,6 +68,23 @@ function indexTasks() {
     .then((response) => response.json())
     .then((data) => renderTask(data))
 }
+function checkLogged() {
+    const elementWhenLoggedIn = document.getElementById("loggedIn")
+    const elementWhenNotLoggedIn = document.getElementById("notLoggedIn")
+
+    fetch("http://127.0.0.1:3000/auth/cookie/status", {
+        credentials: "include"
+    })
+    .then((response) => {
+        if(response.status === 401) {
+            elementWhenLoggedIn.classList.add("hidden")
+            elementWhenNotLoggedIn.classList.remove("hidden")
+        } else {
+            elementWhenLoggedIn.classList.remove("hidden")
+            elementWhenNotLoggedIn.classList.add("hidden")
+        }
+    })
+}
 
 document.addEventListener("DOMContentLoaded", () => {
     const _idInput = document.getElementById("id");
@@ -75,6 +92,19 @@ document.addEventListener("DOMContentLoaded", () => {
     const status = document.getElementById("status");
     
     indexTasks();
+    checkLogged();
+        document.getElementById("login").addEventListener("submit", (event) => {
+        const email = document.getElementById("email_input").value
+        const password = document.getElementById("password_input").value
+        const login = {email, password}
+
+        fetch("http://127.0.0.1:3000/auth/cookie/login", {
+            method: "POST",
+            credentials: "include",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(login)
+        })
+    })
     document.addEventListener('submit', (event) =>{
         createTask({id: _idInput.value, title: todoInput.value, completed: status.value})
 
